15713f5c9f10fe09ce296b5fb308656e6343a3a0
Pu2Pa2 <fu2sak1@gmail.com>
2021-10-12 17:56:50 +0900

    Update IterableIteratorの利用

--- index.ts
+++ index.ts
@@ -13,30 +13,28 @@ class Component {
     constructor(public name: string) {}
 }
 
-class Frame implements Iterable<Component> {
+class Frame implements IterableIterator<Component> {
+    private pointer = 0;
 
     constructor(public name: string, public components: Component[]) {}
-
-    [Symbol.iterator]() {
-        let pointer = 0;
-        let components = this.components;
-
-        return {
-            next(): IteratorResult<Component> {
-                if (pointer < components.length) {
-                    return {
-                        done: false,
-                        value: components[pointer++]
-                    }
-                } else {
-                    return {
-                        done: true,
-                        value: null
-                    }
-                }
+    public next(): IteratorResult<Component> {
+        if (this.pointer < this.components.length) {
+            return {
+                done: false,
+                value: this.components[this.pointer++]
+            }
+        } else {
+            return {
+                done: true,
+                value: null
             }
         }
     }
+
+    [Symbol.iterator](): IterableIterator<Component> {
+        return this;
+    }
+        
 }
 
 let frame = new Frame("Door", [new Component("top"), new Component("bottom"), new Component("left"), new Component("right")]);
