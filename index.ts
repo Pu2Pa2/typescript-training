d981ad43eabbb645eed07282fff7a1c431713079
Pu2Pa2 <fu2sak1@gmail.com>
2021-10-12 11:51:08 +0900

    Update iterableを使ったiterator

--- index.ts
+++ index.ts
@@ -1,46 +1,45 @@
-interface IteratorResult<T> {
-    done: boolean;
-    value?: T;
-}
+// interface IteratorResult<T> {
+//     done: boolean;
+//     value?: T | null;
+// }
 
-interface Iterator<T> {
-    next(value?: any): IteratorResult<T>;
-    return?(value?: any): IteratorResult<T>;
-    throw?(e?: any): IteratorResult<T>;
-}
+// interface Iterator<T> {
+//     next(value?: any): IteratorResult<T>;
+//     return?(value?: any): IteratorResult<T>;
+//     throw?(e?: any): IteratorResult<T>;
+// }
 
 class Component {
     constructor(public name: string) {}
 }
 
-class Frame implements Iterator<Component> {
-    private pointer = 0;
+class Frame implements Iterable<Component> {
 
-    constructor (public name: string, public components: Component[]) {}
+constructor(public name: string, public components: Component[]) {}
 
-    public next(): IteratorResult<Component> {
-        if (this.pointer < this.components.length) {
-            return {
-                done: false,
-                value: this.components[this.pointer++]
+    [Symbol.iterator]() {
+        let pointer = 0;
+        let components = this.components;
+
+        return {
+        next(): IteratorResult<Component> {
+            if (pointer < components.length) {
+                return {
+                    done: false,
+                    value: components[pointer++]
+                }
+            } else {
+                return {
+                    done: true,
+                    value: null
+                }
             }
-        } else {
-            return {
-                done: true
-            }
+        }
         }
     }
 }
 
-let frame = new Frame("Door", [new Component("top"), new Component("bottom"), new Component("left"), new Component("right")])
-let iteratorResult1 = frame.next();
-console.log(iteratorResult1);
-let iteratorResult2 = frame.next();
-console.log(iteratorResult2)
-let iteratorResult3 = frame.next();
-console.log(iteratorResult3);
-let iteratorResult4 = frame.next();
-console.log(iteratorResult4)
-
-let components = iteratorResult1.value
-console.log(components)
+let frame = new Frame("Door", [new Component("top"), new Component("bottom"), new Component("left"), new Component("right")]);
+for (let cmp of frame) {
+    console.log(cmp);
+}
