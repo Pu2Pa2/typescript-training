f229031e92accea2ef98206f87526d672e18dbab
Pu2Pa2 <fu2sak1@gmail.com>
2021-10-15 09:23:09 +0900

    Update タプルの利用

--- index.ts
+++ index.ts
@@ -1,8 +1,2 @@
-function extend<T, U>(first: T, second: U): T & U {
-    return {...first, ...second};
-}
-
-const x = extend({a: 'hello'}, {b: 42});
-
-const a = x.a;
-const b = x.b;
+const tuple: [string, number, string] = ["one", 2, "three"];
+console.log(tuple)
