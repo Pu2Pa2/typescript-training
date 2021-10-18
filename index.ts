370850dfa817289cdeb38ef6655ff8c5715f1d91
Pu2Pa2 <fu2sak1@gmail.com>
2021-10-18 09:43:52 +0900

    Update interfaceの利用

--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
-const tuple: [string, number, string] = ["one", 2, "three"];
-console.log(tuple)
+interface hoge {
+    aaa: number
+    bbb: number
+}
+const hogeVar: hoge = {
+    aaa: 123,
+    bbb: 456
+}
+console.log(hogeVar)
+
+interface hoge {
+    ccc: number
+}
+
+const hogeVar2: hoge = {
+    ccc: 789
+}
+console.log(hogeVar2)
